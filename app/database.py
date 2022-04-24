69e3e630e98e953323d1a62e54aeaa14b93fc0fd
shibing66 <qa4305@gmail.com>
2022-04-24 18:44:22 -0500

    Add environmental varialbe, Fix SQL injection

--- app/database.py
+++ app/database.py
@@ -1,24 +1,33 @@
 import sys
-from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_engine, text
 
-### "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
-def query_db(depth, gradient, conn_str):
-    engine = create_engine(conn_str)
+URL_DB = os.getenv('URL_DB')
 
+def query_db(depth, gradient):
+    print(URL_DB)
+    engine = create_engine(URL_DB)
+
+    query = text("""
+    SELECT latitude, longitude, depth, gradient
+    FROM wells
+    WHERE depth > :depth AND gradient > :gradient
+    """)
+    '''
     query = f"""
     SELECT latitude, longitude, depth, gradient
     FROM wells
     WHERE depth > {depth} AND gradient > {gradient}
     """
-
+    '''
     with engine.connect() as conn:
-        results = conn.execute(query).fetchall()
+        # results = conn.execute(query).fetchall()
+        results = conn.execute(query, depth=depth, gradient=gradient).fetchall()
     
     return results
 
 if __name__== '__main__':
     depth = sys.argv[1]
     gradient = sys.argv[2]
-    conn_str = sys.argv[3]
     
-    print(query_db(depth, gradient, conn_str))
+    print(query_db(depth, gradient))
